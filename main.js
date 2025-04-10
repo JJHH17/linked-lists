20d9f78f2054984dd3c4adb2dc4a454a6e4b656c
James Hatfield <james.hatfield17@icloud.com>
2025-04-10 10:00:42 +0100

    Completed linked list object

--- main.js
+++ main.js
@@ -2,20 +2,11 @@ import { LinkedList, Node } from "./linkedList.js";
 
 const list = new LinkedList();
 
-
-list.append("cat");
 list.append("dog");
-list.append("animal");
-list.prepend("wolf");
-list.append("bird");
-
-list.contains("hello");
-list.contains("dog");
-
-list.find("cat");
-
-list.toString();
-
-list.removeAt(1);
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
 list.toString();
